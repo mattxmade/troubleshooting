5b538cd72306c47ba244ddb0718144a5e6fbb16f
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-25 03:41:38 +0000

    Commit all files

--- webpack.common.js
+++ webpack.common.js
@@ -8,30 +8,45 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      minify: false,
+      minify: true,
       title: "Troubleshooting",
       template: "./src/template.html",
     }),
     new CopyPlugin({
-      patterns: [{ from: "./public", to: path.resolve(__dirname, "./dist") }],
+      patterns: [{ from: "./public", to: path.resolve(__dirname, "dist") }],
     }),
   ],
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    assetModuleFilename: "[name][ext]",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: (pathData) => {
+      const filepath = path
+        .dirname(pathData.filename)
+        .split("/")
+        .slice(1)
+        .join("/");
+      return `${filepath}/[name][ext]`;
+    },
     clean: true,
   },
   optimization: {
-    runtimeChunk: "single",
+    //runtimeChunk: "single",
+
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        target: "es2015",
+      }),
+    ],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        loader: "esbuild-loader",
+        options: {
+          loader: "jsx",
+          target: "es2015",
         },
       },
       {
@@ -39,11 +54,13 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|webp|ico)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp|ico|fbx|gltf|glb|hdr|mp4|mp3|txt)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
     ],
   },
 };
-
-// [1] dist structure === src strucutre https://stackoverflow.com/a/68902490
